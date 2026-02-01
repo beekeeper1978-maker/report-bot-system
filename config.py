81b6c1a40b836e3f5df5055c99f1ca6c1c522faf
beekeeper1978-maker <beekeeper1978@gmail.com>
2026-02-01 23:02:09 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BOT_TOKEN = "8446705525:AAH8evf1zy3QXKj-fJh2cc_KdM-OA2rFaBw"
-WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbxT3h3EyOhJDhRur2dFHQDqiv0n7aZ-gUYnrkoUM8RMm2362D85knNJJrA8fXF1Gn8G-A/exec"
+WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbxXsUoqtB5fbgiRkFS20b7sod8F_htku_qkpCVcCPfrHdTkaADKuhhiK7kSozmhiQv_/exec"
 
 QUESTIONS = [
     "1. Отчётный период (например: 10-16 марта 2025)",
