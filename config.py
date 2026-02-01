668e3cefa036fbc149d919f44df429b2bf56c31b
beekeeper1978-maker <beekeeper1978@gmail.com>
2026-02-01 22:53:46 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 BOT_TOKEN = "8446705525:AAH8evf1zy3QXKj-fJh2cc_KdM-OA2rFaBw"
-WEBHOOK_URL = "https://script.google.com/macros/s/AKfycby5lCPSnWo5aDUZ5o5EY-FpDd69apIrzQla4XEyoY4H1a9Fac4PvnJ_nqVmtrjRGGRrqg/exec"
+WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbxT3h3EyOhJDhRur2dFHQDqiv0n7aZ-gUYnrkoUM8RMm2362D85knNJJrA8fXF1Gn8G-A/exec"
 
 QUESTIONS = [
     "1. Отчётный период (например: 10-16 марта 2025)",
     "2. Автор отчёта (ФИО)",
-    "3. Подразделение/отдел",
+    "3. Подразделение",
     "4. Ключевые результаты недели (по пунктам)",
-    "5. Проблемы и сложности",
-    "6. Решенные проблемы",
-    "7. Что нужно от руководства/коллег",
-    "8. Оценка недели (от 1 до 10)",
+    "5. Что проверено по чек-листу аудита",
+    "6. Номера задач в Битрикс24",
+    "7. Финансы (куплено/отремонтировано)",
+    "8. Пункты KPI, требующие внимания",
     "9. План на следующую неделю"
 ]
